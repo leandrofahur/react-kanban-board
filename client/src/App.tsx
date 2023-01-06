798bfa72b32a08d1205a8540fba64b6e8c1f94ef
leandro <fahurleandro@gmail.com>
2023-01-05 20:12:26 -0800

    Styled components operational

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const AppContainer = styled.div`
+  background-color: #333;
+  color: #fff;
+`
+
+export function App() {
+  return (
+    <AppContainer>APP COMPONENT</AppContainer>
+  )
+}
