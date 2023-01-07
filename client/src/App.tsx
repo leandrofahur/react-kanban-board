b504507ce20f2f9a26e50f701b4a1bcd61bfe1d4
leandro <fahurleandro@gmail.com>
2023-01-07 13:17:53 -0800

    added basic GlobalStyles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
-import styled from "styled-components"
-
-const AppContainer = styled.div`
-  background-color: #333;
-  color: #fff;
-`
-
 export function App() {
   return (
-    <AppContainer>APP COMPONENT</AppContainer>
+    <div>APP COMPONENT</div>
   )
 }
