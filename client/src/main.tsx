5809dc4614a1bb21b6b9c86deb1b4b562ecf9321
leandro <fahurleandro@gmail.com>
2023-01-07 13:27:46 -0800

    added basic defaultTheme, adapted the styled.d.ts, and wrapped the theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyles/>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ client/src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colors: {
+    white: "#ffffff",
+    black: "#000000",
+
+    background: "#e1e8ed",
+
+    primary: "#171717",
+
+    done: "#59d090",
+    review: "#945ad1",
+    inProgress: "#d0935a",
+    toDo: "#59b4d1"
+  }
+}
